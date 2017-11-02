58802e835f44a3a10110de971ad9275881df96fa
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-11-03 00:50:28 +0530

    extract paths in server

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var { BUILD_DIR, PUBLIC_DIR } = require('./paths');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -20,7 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(BUILD_DIR));
+app.use(express.static(PUBLIC_DIR));
 
 app.use('/', index);
 app.use('/users', users);
---
+++ server/paths.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const BUILD_DIR = path.join(__dirname, '..', 'client', 'build');
+const PUBLIC_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  BUILD_DIR,
+  PUBLIC_DIR,
+};

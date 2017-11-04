a709c40623aa35453d46b7fb15a4caae34cdb25b
Shirsh Zibbu <zibbu.shirsh+github@gmail.com>
2017-11-04 16:09:24 +0530

    add redux to client/

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { resetName, updateName } from './reducer';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleChange = (e) => {
+    this.props.updateName(e.currentTarget.value);
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,9 +21,29 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-intro">
+          <input
+            onChange={this.handleChange}
+            placeholder="Name"
+            value={this.props.name}
+          />
+          <button onClick={this.props.resetName}>×</button>
+        </p>
+        <p className="App-intro">
+          Hi! My name is <strong>{this.props.name || '…'}</strong>
+        </p>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  const name = state;
+  console.log(state)
+
+  return { name };
+}
+
+const mapDispatchToProps = { resetName, updateName };
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
